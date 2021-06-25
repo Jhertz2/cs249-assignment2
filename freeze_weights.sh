6741f42dad84556cee3c30e340cf83df8a68e209
Jacob Hertz <jhertz2@u.rochester.edu>
2021-06-25 13:11:02 -0400

    Update freeze_weights.sh

--- freeze_weights.sh
+++ freeze_weights.sh
@@ -1,7 +1,12 @@
-python freeze_graph.py \
-    --input_graph=vww_96_grayscale_graph.pb \
-    --input_checkpoint="C:\workspace\cs249-assignment2-module-2\vww_96_grayscale\model.ckpt-463859" \
+python /content/cs249-assignment2/freeze_graph.py \
+    --input_graph=/content/vww_96_grayscale_graph.pb \
+    --input_checkpoint=/content/transfer_learning/model.ckpt-1005000 \
     --input_binary=true \
-    --output_graph=person_detection_frozen.pb \
+    --output_graph=/content/person_detection_frozen.pb \
     --output_node_names=MobilenetV1/Predictions/Reshape_1
+
+
+    # input graph is output of export_graphdef.sh
+    #checkpoint will be last checkpoint from transferlearning.sh
+    
 
