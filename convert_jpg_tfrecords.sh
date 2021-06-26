390efded1c2bedad27bcc8cfffd808d98e7637cc
Jacob Hertz <jhertz2@u.rochester.edu>
2021-06-26 15:58:22 -0400

    Update convert_jpg_tfrecords.sh

--- convert_jpg_tfrecords.sh
+++ convert_jpg_tfrecords.sh
@@ -1,3 +1,3 @@
  python /content/cs249-assignment2/datasets/download_and_convert_data.py \
     --dataset_name=custom \
-    --dataset_dir=/content/pill_data
+    --dataset_dir=/content/gdrive/MyDrive/pill_data
