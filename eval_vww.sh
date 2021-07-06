cd0b1a624433d19f87640055dd1815726e71e734
Jacob Hertz <jhertz2@u.rochester.edu>
2021-07-05 20:57:05 -0400

    Update eval_vww.sh

--- eval_vww.sh
+++ eval_vww.sh
@@ -1,7 +1,7 @@
- python eval_image_classifier.py \
+ python /content/cs249-assignment2/eval_image_classifier.py \
     --alsologtostderr \
-    --checkpoint_path=vww_96_grayscale \
-    --dataset_dir=C:\workspace\cs249-assignment2-module-2\data\visualwakewords \
+    --checkpoint_path=/content/gdrive/MyDrive/transfer_learning \
+    --dataset_dir=/content/gdrive/MyDrive/pill_data \
     --dataset_name=visualwakewords \
     --dataset_split_name=val \
     --model_name=mobilenet_v1_025 \
